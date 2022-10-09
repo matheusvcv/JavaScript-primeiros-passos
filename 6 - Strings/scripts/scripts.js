d64683a2c9216229e3a1c1ee564713ff5de357f3
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-09 15:32:51 -0300

    Aprendendo a Utilizar strings em JavaScript

---
+++ 6 - Strings/scripts/scripts.js
@@ -0,0 +1,24 @@
+var nome = "Matheus";
+console.log(nome);
+console.log(typeof nome);
+
+var sobrenome = "Viana";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+var numero = "523";
+console.log(numero);
+console.log(typeof numero);
+
+var frase = 'Essa é uma frase completa';
+console.log(frase);
+console.log(typeof frase);
+
+document.write("Olá, mundo!");
+
+var cersei = 'Então Cersei respondeu "I choose violence"';
+
+console.log(cersei);
+document.write(cersei);
