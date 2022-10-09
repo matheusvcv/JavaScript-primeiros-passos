63cddc09d3d20e664a56d7d4d110a8caa2c9a522
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-09 15:53:03 -0300

    Aprendendo a trbalhar com variáveis do tipo Boolean em JavaScript

---
+++ 7 - Booleans/scripts/scripts.js
@@ -0,0 +1,10 @@
+var verdadeiro = true;
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+var falso = false;
+console.log(falso);
+console.log(typeof falso);
+
+console.log(typeof false);
+console.log(typeof true);
