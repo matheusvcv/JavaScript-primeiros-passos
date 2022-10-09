4744db97124016c3814cb93ceef9ddc6f4ac79bc
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-09 14:36:54 -0300

    Conhecendo o datatype number

---
+++ 5 - Number/scripts/scripts.js
@@ -0,0 +1,21 @@
+var numero = 5;
+
+console.log(numero);
+
+console.log(typeof numero);
+
+var float = 5.5;
+
+console.log(float);
+
+console.log(typeof float);
+
+var textoComNumero = "523";
+
+console.log(textoComNumero);
+
+console.log(typeof textoComNumero);
+
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
