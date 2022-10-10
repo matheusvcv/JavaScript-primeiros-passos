e6a3a53df6c59d2be0993ecc5e25f4f889a34ecf
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-10 17:02:22 -0300

    Trabalhando com else e else if no JS

---
+++ 13 - else e else if/scripts/scripts.js
@@ -0,0 +1,24 @@
+var nome = "Xavier";
+
+if(nome == "Pedro"){
+	console.log("O nome é Pedro");
+
+} else if(nome == "Matheus"){
+	console.log("O nome é Matheus.");
+
+} else if(nome == "Severo"){
+	console.log("O nome é Severo.");
+
+}else{
+	console.log("O nome não é nem Pedro, nem Matheus e  nem Severo");
+}
+
+var idade = 19;
+
+if(idade >= 20){
+	console.log("Pode entrar sozinho!");
+}else if (idade == 19){
+	console.log("Pode entrar acompanhado");
+} else {
+	console.log("Não pode entrar de jeito nenhum!");
+}
