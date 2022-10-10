88e4089bd414488a3481690048151e3436b9b9fa
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-10 15:33:33 -0300

    Trabalhando com operadores condicionais em JS

---
+++ 12 - Operadores de Comparação/scripts/scripts.js
@@ -0,0 +1,46 @@
+var idade = 18;
+var idade2 = 17;
+
+if(idade >= 18){
+
+	console.log("Pode fazer a carteira.");
+}
+
+if(idade2 <= 17) {
+	console.log("Não pode tirar a carteira");
+}
+
+var possuiCarro = 1;
+var naoPossuiCarro = 0;
+
+if(possuiCarro){
+
+	console.log("Tem carro");
+}
+
+if(naoPossuiCarro){
+
+	console.log("Não tem carro.");
+}
+
+var nome = "Matheus";
+
+if(nome == "Matheus"){
+
+	console.log("Seu nome é Matheus.");
+}
+
+if(nome != "Pedro"){
+
+	console.log("Seu nome não é Pedro.");
+}
+
+if(100 < 20){
+
+	console.log("Entrou.");
+}
+
+if(20 > 10){
+
+	console.log("Entrou**");
+}
