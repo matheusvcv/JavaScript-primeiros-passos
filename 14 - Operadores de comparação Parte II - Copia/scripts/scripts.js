e9e179656840f6ea19a580fdcbb1554050bac7b0
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-10 17:32:55 -0300

    Aprendendo a trabalhar os operadores lógicos === e !== em JavaScript

---
+++ 14 - Operadores de comparação Parte II - Copia/scripts/scripts.js
@@ -0,0 +1,23 @@
+var numero = "5";
+
+if(numero == 5){
+
+	console.log("Os números coincidem!");
+} 
+
+if(numero === 5){
+
+	console.log("Os números coincidem");
+}
+
+var numeroP = "2";
+
+if(numeroP =! 2){
+
+	console.log("São diferentes!");
+}
+
+if (numeroP ==! 2){
+
+	console.log("São diferentes demais");
+}
