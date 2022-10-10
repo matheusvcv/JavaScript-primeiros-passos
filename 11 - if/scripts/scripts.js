8e3376327e2ef4a7777e8d7d817bebb044c78f3f
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-10 15:01:18 -0300

    Trabalhando estruturas condicionais em JavaScript

---
+++ 11 - if/scripts/scripts.js
@@ -0,0 +1,24 @@
+console.log("Antes");
+
+if(false){
+	console.log("Testando o if.");
+}
+
+console.log("Depois");
+
+var idade = 19;
+var idade2 = 15;
+
+if(idade >= 18){
+
+	console.log("Pode tirar carteira de motorista.");
+
+} else{
+
+	console.log("Não pode tirar carteira.");
+}
+
+if(idade2 === 15) {
+
+	console.log("Ainda precisa esperar 3 anos para estar com a idade adequada.");
+}
