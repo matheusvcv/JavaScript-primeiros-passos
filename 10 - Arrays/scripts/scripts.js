17ee00e6744bd9f5aae1bd2c3ec587bf2a7106bb
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-10 12:21:52 -0300

    Aprendendo a trabalhar com Arrays em JS

---
+++ 10 - Arrays/scripts/scripts.js
@@ -0,0 +1,20 @@
+var arr = [5, "Matheus", true, {teste: 1, teste2: 2, testeString: "String"}];
+console.log(arr);
+
+var arr2 = [1, 2, 3, 4, 5, 6];
+console.log(arr2);
+
+
+console.log(arr[1]);
+
+console.log(arr2[4]);
+
+console.log(arr2[0]);
+
+arr[6] = [10];
+arr[2] = "Coelho";
+
+arr[1] = "Rick";
+
+console.log(arr);
+console.log(typeof arr);
