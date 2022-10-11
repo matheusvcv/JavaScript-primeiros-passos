17bbd014b6a78abf3a1fe9b31d32ebc0073d180a
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-11 17:16:50 -0300

    Trabalhando com estrutura de repetição While

---
+++ 18 - While/scripts/scripts.js
@@ -0,0 +1,34 @@
+var x = 0;
+
+while(x < 5){
+
+	console.log("Testando repetição " + x);
+
+	x++;
+}
+
+
+var arr = ['Corlys','Viserys','Rhaenyra','Daemon','Aemond'];
+
+var y = 0;
+
+
+while(y <= 4){
+
+	console.log(arr[y]);
+
+	y++;
+}
+
+
+var palavra = "Matheus";
+
+var i = 0;
+
+while(i < 7){
+
+	console.log(palavra[i]);
+
+	i++;
+
+}
