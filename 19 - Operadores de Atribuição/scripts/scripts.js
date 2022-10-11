cf5a3217d46704580e6cf29d372b690ef71eda04
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-11 18:08:27 -0300

    Trabalhando com novos operadores de atribuição

---
+++ 19 - Operadores de Atribuição/scripts/scripts.js
@@ -0,0 +1,65 @@
+var x = 1;
+var y = 2;
+
+//Adição - Sem resumir
+x = x + y;
+console.log(x);
+
+//Adição - Resumindo
+x += y;
+console.log(x);
+
+var a = 10;
+var b = 20;
+
+//Subtração - Sem resumir
+b = b - a;
+console.log(b);
+
+//Subtração - Resumindo
+b -=a;
+console.log(b);
+
+//Multiplicação - Sem resumir
+x = x * y;
+console.log(x);
+
+//Multiplicação - Resumindo
+x *= y;
+console.log(x);
+
+
+//Divisão - Sem resumir
+x = x / y;
+console.log(x);
+
+//Divisao - Resumindo
+x /= y;
+console.log(x);
+
+//Loops
+
+z = 0;
+
+j = 1;
+
+while(z <= 100){
+
+	console.log(z);
+
+	z++;
+}
+
+while(j <= 100){
+
+	console.log(j);
+
+	j *= 2;
+}
+
+while(z >= 0){
+
+	console.log(z);
+
+	z -= 5;
+}
