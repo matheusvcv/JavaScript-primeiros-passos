74217b3e84b287602a6ea7e4571c4dc3d892d508
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-11 18:39:35 -0300

    Aprendendo a trabalhar com a estrutura de repetição for

---
+++ 20 - FOR/scripts/scripts.js
@@ -0,0 +1,13 @@
+for(var i = 0; i<=10; i++){
+	console.log("Teste " + i);
+}
+
+var arr = ['Walt', 'Jesse', 'Hank', 'Mike', 'Saul', 'Katniss'];
+
+for(x = 0; x < arr.length; x++){
+	console.log(arr[x]);
+}
+
+for(y = 5; y < 100; y *= 3){
+	console.log(y);
+}
