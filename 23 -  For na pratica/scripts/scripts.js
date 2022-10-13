c076204c8fba5b63ef642641533f3f18a2a5a041
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-13 20:32:57 -0300

    Praticando JavaScript

---
+++ 23 -  For na pratica/scripts/scripts.js
@@ -0,0 +1,28 @@
+var lista = ['Robert', 'Jofrey', 'Tomem', 'Aerys', 'Aegon'];
+
+var listaUl = document.createElement('ul');
+
+var body = document.getElementsByTagName('body');
+
+console.log(body);
+
+console.log(body[0]);
+
+body[0].appendChild(listaUl);
+
+var listaNoBody = document.getElementsByTagName('ul');
+
+console.log(listaNoBody[0]);
+
+
+for(var i = 0; i < lista.length; i++){
+
+	var liFor = document.createElement('li');
+
+	var textoLi = document.createTextNode(lista[i]);
+
+	liFor.appendChild(textoLi);
+
+	listaNoBody[0].appendChild(liFor);
+	
+}
