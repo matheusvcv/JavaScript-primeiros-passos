a063f583d6ee4d712b6e882e22a4019655cd10a6
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-10-14 10:58:10 -0300

    Aprendendo escopos locais e global em JavaScript

---
+++ 24 - Escopos/scripts/scripts.js
@@ -0,0 +1,24 @@
+var x = 1;
+var y = 3;
+
+console.log(x, y);
+
+function nome()
+{
+	var z = 0;
+
+	console.log(z);
+
+	console.log(x);
+}
+
+nome();
+
+function testando ()
+{
+	var z = 5;
+
+	console.log(z);
+}
+
+testando();
