6b775f09b6ea3a36005600e4c244d7b2b83753ad
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-11-14 12:50:14 -0300

    Realizados exercícios de prática da estrutura condicional While

--- 18 - While/scripts/scripts.js
+++ 18 - While/scripts/scripts.js
@@ -32,3 +32,45 @@ while(i < 7){
 	i++;
 
 }
+
+
+var jota = 1;
+
+
+while(jota < 10){
+
+	console.log("Testando repetição Jota " + jota);
+
+	jota++;
+}
+
+var brasil = 1;
+
+while(brasil <= 5){
+
+	console.log("O Brasil ganhou a copa " + brasil + " vezes!");
+
+	brasil++;
+}
+
+var arr = ['Rick', 'Shane', 'Carl', 'Daryl'];
+
+var y = 0;
+
+while(y <= 3){
+
+	console.log(arr[y]);
+
+	y++;
+}
+
+var dog = "Cachorro";
+
+var d = 0;
+
+while(d <= 8){
+
+	console.log(dog[d]);
+
+	d++;
+}
