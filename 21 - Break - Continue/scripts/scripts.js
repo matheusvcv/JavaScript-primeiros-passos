bd13de21f2cf6df7879c23e855acfa1e78ec5bed
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-11-16 16:03:16 -0300

    Realizadas práticas com os conceitos de Break e Continue

--- 21 - Break - Continue/scripts/scripts.js
+++ 21 - Break - Continue/scripts/scripts.js
@@ -25,3 +25,46 @@ while(x < 100){
 
 
 }
+
+
+for(var kakaroto = 8000; kakaroto > 0; kakaroto--){
+
+	console.log(kakaroto);
+
+	if(kakaroto === 7900){
+
+		break;
+	}
+}
+
+console.log("Deu break");
+
+for(var idade = 30; idade > 0; idade--)
+{
+	console.log(idade);
+
+	if(idade === 24){
+
+		break;
+	}
+}
+
+console.log("Deu break");
+
+
+var ano = 10;
+
+while(ano < 100){
+
+	ano += 10;
+
+	if(ano === 90 || ano === 60){
+
+		console.log("Pulou");
+
+		continue;
+
+	}
+
+	console.log("Testando " + ano);
+}
