7ae90fbd881dad3071840aa496f6c3f75f63da06
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-11-29 15:07:50 -0300

    Praticas do metodos de array splice, indexOf, join e Reverse em JavaScript

---
+++ 30 - Métodos do Array pt-2/scripts/scripts.js
@@ -0,0 +1,30 @@
+//splice
+
+var arr = ["Eddard","Robb","Bran","Ricon", "Sansa", "Arrya"];
+
+
+arr.splice(4, 0, "Jon");
+
+
+console.log(arr);
+
+arr.splice(0, 1);
+
+console.log(arr);
+
+//indexOf
+
+console.log(arr.indexOf("Jon"));
+
+//join
+
+arr2 = ["O", "rato", "roeu", "a", "roupa", "do", "rei", "de", "Roma"];
+
+console.log(arr2);
+
+console.log(arr2.join(" "));
+
+//reverse
+
+console.log(arr.reverse());
+console.log(arr2.reverse());
