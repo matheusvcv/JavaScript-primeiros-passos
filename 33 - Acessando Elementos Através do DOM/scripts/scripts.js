33d0e5503dcc71c0cd53cd3814cecf998441b7fa
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-11-30 15:05:03 -0300

    Exercícios praticos sobre o acesso de elementos através de métodos DOM. Métodos utilizados: getElementsByTagName, getElementById, getElementsByClass

---
+++ 33 - Acessando Elementos Através do DOM/scripts/scripts.js
@@ -0,0 +1,14 @@
+var titulo = document.getElementsByTagName('h1')[0];
+console.log(titulo);
+
+var lis = document.getElementsByTagName('li');
+console.log(lis);
+
+console.log(lis[3]);
+
+
+var paragrafo = document.getElementById("paragrafo");
+console.log(paragrafo);
+
+var itensDaLista = document.getElementsByClassName('item');
+console.log(itensDaLista);
