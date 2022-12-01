53801babf64b1d1c3ff7112c1d1a4b6370ef9748
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-12-01 17:34:07 -0300

    Praticas de manipulação de DOM com os querySelector e querySelectorAll

---
+++ 34 - querySelector/scripts/scripts.js
@@ -0,0 +1,17 @@
+//querySelectorAll()
+
+var itens = document.querySelectorAll('#lista2 li');
+
+console.log(itens);
+
+var itens2 = document.querySelectorAll('#lista .item');
+console.log(itens2);
+
+var itens3 = document.querySelector('#lista2 .item');
+console.log(itens3);
+
+var lista = document.querySelector('#lista');
+console.log(lista);
+
+var ul = document.querySelector('ul');
+console.log(ul);
