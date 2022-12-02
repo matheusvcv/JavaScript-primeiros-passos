9bd0ab807b350529dfd32a99c5be644a9aad8536
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-12-02 10:23:32 -0300

    Utilizando o innerHTML e o textContent para alterar os conteúdos de texto das tags html.

---
+++ 35 - Alterar Conteúdo/scripts/scripts.js
@@ -0,0 +1,11 @@
+//innerHTML
+
+title.innerHTML = "Novo título";
+
+var title = document.querySelector('#title');
+
+//textContent
+
+var subtitle = document.querySelector('.subtitle');
+
+subtitle.textContent = "Testando textContent";
