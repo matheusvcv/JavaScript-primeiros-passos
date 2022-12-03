1f32877a2cb8f276a50e533a99a5419423c68ee5
Matheus Vinicius <matheusvcv@yahoo.com.br>
2022-12-03 16:11:48 -0300

    Praticas utilizando os metodos remove() e removeChild() para remover elementos do HTML

---
+++ 37 - Remover Elementos/scripts/scripts.js
@@ -0,0 +1,10 @@
+
+var container = document.querySelector('#container');
+
+var paragrafo = document.querySelector('#container p');
+
+container.removeChild(paragrafo);
+
+var subtitle = document.querySelector('.subtitle');
+
+subtitle.remove();
